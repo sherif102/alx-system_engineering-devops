9c495b24965f405781f3a186df7ad62f7580885d
hyperbayse <hyperbayse@gmail.com>
2025-02-03 13:28:29 +0100

    checkout correction on Werkzeug version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # install a package flask from pip3
-package { 'flask':
+package { ['flask', 'werkzeug']:
     ensure   => '2.1.0',
     provider => 'pip3',
+}
+
+exec { 'install_compatible_wekzeug':
+    command => 'pip3 install "Werkzeug<2.1"',
+    unless  => 'pip3 list | grep Werkzeug | grep 2.0',
 }

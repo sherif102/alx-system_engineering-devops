578999e241ac65c8df20d20fd7f87a9450a39e9b
hyperbayse <hyperbayse@gmail.com>
2025-02-04 23:34:02 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,12 @@
 # setup an ssh configuration file named ~/.ssh/school
+file { '${HOME}/.ssh':
+    ensure => 'directory',
+    mode    => '0700',
+}
+
 file { '${HOME}/.ssh/ssh_config':
     ensure  => 'file',
     mode    => '0600',
     content => 'Host 525397-web-01\n\tHostName 54.175.225.156\n\tUser ubuntu\n\tIdentityFile ~/.ssh/school\n\tIdentitiesOnly yes\n\tPasswordAuthentication no',
+    require => File['${HOME}/.ssh'],
 }

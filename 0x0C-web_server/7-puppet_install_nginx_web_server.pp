8733c813e170ce95d7fbea0d9486e5868f96b981
hyperbayse <hyperbayse@gmail.com>
2025-02-06 13:06:38 +0100

    correction

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,25 @@
 # configures nginx server and do some basic setups
-package { 'nginx':
-    ensure => installed,
+exec { 'install nginx server':
+    command => 'sudo apt-get -y install nginx',
 }
 
-file { '/var/www/html/index.html':
+file { 'index.html':
     ensure  => file,
     content => 'Hello World!',
-    mode    => '0644',
+    mode    => '0744',
 }
-file { '/var/www/html/redirect_me':
+file { 'redirect_me':
     ensure  => file,
     content => 'Redirection',
-    mode    => '0644',
+    mode    => '0744',
 }
 
-exec { 'listen on 80':
-    command => ['sudo ufw allow "Nginx HTTP"',
+exec { 'move the index.html and redirect_me to /var/www/html/':
+    command => ['sudo mv index.html /var/www/html/',
+    'sudo mv redirect_me /var/www/html/',
+    'sudo ufw allow "Nginx HTTP"',
     'sudo cp /etc/nginx/sites-available/default ./',
     ],
-    path    => ['/bin', '/usr/bin'],
 }
 
 exec { 'adjust the server block and update':
@@ -27,10 +28,8 @@ exec { 'adjust the server block and update':
     rewrite ^/redirect_me shafam.tech permanent;" default',
     'sudo mv default /etc/nginx/sites_available/',
     ]
-    path    => ['/bin', '/usr/bin'],
 }
 
 exec { 'restart the nginx server':
     command => 'sudo system nginx restart',
-    path    => ['/bin', '/usr/bin'],
 }

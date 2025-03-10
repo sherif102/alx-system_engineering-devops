9e23c6a84fe7c091164b82b6618e58544446c72f
hyperbayse <hyperbayse@gmail.com>
2025-03-10 14:45:51 +0100

    format correction

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,16 +15,10 @@ def todo_getter(employee_id):
         f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
     todos = json.loads(todos.read().decode('utf-8'))
-    employee_name = eval(name_request.read().decode('utf-8'))['name']
-    number_of_done_tasks = 0
-    total_number_of_tasks = 0
-    # task_done_list = []
-    # all_tasks = []
+    employee_name = eval(name_request.read().decode('utf-8'))['username']
 
     with open(f'{employee_id}.csv', 'w') as file:
         for task in todos:
-            # data = f'"{task['userId']}", "{employee_name}", \
-            # "{task['completed']}", "{task['title']}"\n'
             data = '"{}", "{}", "{}", "{}"\n'.format(
                 task['userId'],
                 employee_name,
@@ -32,12 +26,6 @@ def todo_getter(employee_id):
                 task['title']
             )
             file.write(data)
-
-    # print(f'Employee {employee_name} is done with tasks'
-        #   f'({number_of_done_tasks}/{total_number_of_tasks}):')
-
-    # for task in task_done_list:
-    #     print(f'\t {task}')
 
 
 if __name__ == "__main__":

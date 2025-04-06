e53fb1f9256b76baf10830215e3f020f223399d2
hyperbayse <hyperbayse@gmail.com>
2025-04-07 00:15:13 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,6 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     h = {'User-Agent': 'local'}
     request = requests.get(url, allow_redirects=False, headers=h)
+    if request.status_code != 200:
+        return 0
     return request.json()["data"]["subscribers"]
-
-# print(number_of_subscribers("programming"))

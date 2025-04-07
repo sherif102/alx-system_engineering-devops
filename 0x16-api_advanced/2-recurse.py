2f8192563a014fdef69823a6017b6611cd7ea35e
hyperbayse <hyperbayse@gmail.com>
2025-04-07 08:16:08 +0100

    style update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,13 +4,13 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], counter=0):
-    if type(subreddit) != list:
+    if type(subreddit) is not list:
         url = f"https://www.reddit.com/r/{subreddit}.json"
         h = {'User-Agent': 'local'}
         request = requests.get(url, allow_redirects=False, headers=h)
 
         if request.status_code != 200:
-            return "not 200"
+            return None
         subreddit = request.json()["data"]["children"]
 
     hot_list.append(subreddit[counter]["data"]["title"])
@@ -19,5 +19,3 @@ def recurse(subreddit, hot_list=[], counter=0):
     if counter != len(subreddit):
         return recurse(subreddit, hot_list, counter)
     return hot_list
-
-print(recurse("programming"))
